5a50008e3e7a1008a5837ecca6a8a154cc60c5f0
bmkun <bmkun.dot@gmail.com>
2018-12-12 19:00:35 +0700

    hanya interface

---
+++ src/hafidh/Hafidh.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hafidh;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author KEMPIT
+ */
+public class Hafidh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+//     Scanner input = new Scanner(System.in);
+     
+//     int input1 = input.nextInt();
+     
+   
+           login loginKasir = new login();
+           loginKasir.setVisible(true);
+           
+           
+           
+    
+        
+     
+     
+     
+        
+
+     
+   
+     
+     
+    }
+    
+}
